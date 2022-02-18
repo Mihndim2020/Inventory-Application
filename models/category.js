9047e230b1468c4b81e04b14908a9e65e2eae9b4
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-02-17 23:48:34 -0500

    Add Item model

--- models/category.js
+++ models/category.js
@@ -2,16 +2,16 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var CategorySchema = new Schema (
+var CategorySchema = new Schema(
     {
         name: {type: String, required: true, maxLength: 100},
         description: {type: String, required: true, maxLength: 100},
     }
 );
 
-CategorySchema.virtual('url').get(function () 
+CategorySchema.virtual('url').get(function() 
 {
-    return '/category/' + this._id;
+    return '/inventory/category/' + this._id;
 });
 
 module.exports = mongoose.model('Category', CategorySchema);
