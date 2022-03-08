2023f2a5edd0508c50530557f2df2d4369ccb7ec
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-03-08 01:09:43 -0500

    Implement add item controller functions

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,9 @@
 var Item = require('../models/item');
+var Category = require('../models/category');
 
 var async = require('async');
+
+const { body,validationResult } = require('express-validator');
 
 // Display list of all items.
 exports.item_list = function(req, res, next) {
@@ -36,13 +39,84 @@ exports.item_detail = function(req, res) {
 
 // Display item create form on GET.
 exports.item_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Item create GET');
-};
+    async.parallel({
+        categories: function(callback) {
+            Category.find(callback);
+        }
+    }, function(err, results) {
+        if (err) { return next(err); }
+        res.render('item_form', { title: 'Create Item', categories: results.categories });
+    });
+    // categories: Category.find({}, function(err, results){
+    //         if (err) { return next(err); }
+    //         res.render('item_form', { title: 'Create Item', categories: results.categories });
+    //     })
+        
+    };
 
 // Handle item create on POST.
-exports.item_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Item create POST');
-};
+exports.item_create_post = [
+    // Validate and sanitize fields
+     body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+     body('tradeMark', 'Trademark must not be empty.').trim().isLength({ min: 1 }).escape(),
+     body('category', 'Summary must not be empty.').trim().isLength({ min: 1 }).escape(),
+     body('price', 'Price must not be empty').trim().isLength({ min: 1 }).escape(),
+     body('numberInStock', 'numberInStock must not be empty').trim().isLength({ min: 1 }).escape(),
+     body('speed').optional({ checkFalsy: true }).trim().isLength({ min: 1 }).escape(),
+     body('capacity').optional({ checkFalsy: true }).trim().isLength({ min: 1 }).escape(),
+     body('dimension').optional({ checkFalsy: true }).trim().isLength({ min: 1 }).escape(),
+     body('imageUrl').optional({ checkFalsy: true }).trim().isLength({ min: 1 }).escape(),
+
+     // Process the request after validation and sanitation
+
+     (req, res, next) => {
+
+        // Extract validation errors from the request
+        const errors = validationResult(req);
+
+        // Create an Item object with escaped and trimmed data. 
+        var item = new Item(
+          {
+            name: req.body.name,
+            tradeMark: req.body.tradeMark,
+            category: req.body.category,
+            price: req.body.price,
+            numberInStock: req.body.numberInStock,
+            speed: req.body.speed,
+            capacity: req.body.capacity,
+            dimension: req.body.dimension,
+            imageUrl: req.body.imageUrl
+          });
+
+          if(!errors.isEmpty()) {
+              // There are errors, Render the form again with sanitized values/error messages. 
+              async.parallel({
+                categories: function(callback) {
+                    Category.find(callback);
+                }
+            }, function(err, results) {
+                if (err) { return next(err); }
+                res.render('item_form', { title: 'Create Item', categories: results.categories, errors: errors.array() });
+            });
+            return;
+          }
+
+          else {
+              // Data from the form is valid, Save Item. 
+              item.save(function(err){
+                  if (err) {return next(err);}
+                  // successful, redirect to the new item record
+                  res.redirect(item.url);
+              });
+
+          }
+
+
+
+     }
+     
+   
+];
 
 // Display item delete form on GET.
 exports.item_delete_get = function(req, res) {
